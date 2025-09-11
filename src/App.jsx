23d5d4f7de0449f94949192cb027f84c6c0d7b3e
Anh <qanh171002@gmail.com>
2025-09-11 21:55:51 +0700

    add main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
-  return <div>Hotel Management</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/bookings" element={<Bookings />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
